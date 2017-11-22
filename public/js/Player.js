d79cd283f46082354035e425335e53fc1c578f1f
juancgarcia <juangarcia.info@gmail.com>
2017-11-22 16:55:29 -0500

    refactor Player templatte to use computed value for score

--- public/js/Player.js
+++ public/js/Player.js
@@ -2,7 +2,22 @@
 Vue.component('player', {
   props: ['playerSelection'],
   template: `<div class="player">
-    ({{playerSelection.username}})<div class="action">{{playerSelection.choice}}</div>
-    <div>{{(playerSelection.score === undefined)? ("") : (playerSelection.score > 0) ? "Result: Winner!":"Result: Lose!"}}</div>
-  </div>`
+    ({{playerSelection.username}})
+    <div class="action">{{playerSelection.choice}}</div>
+    <div>{{score}}</div>
+  </div>`,
+  computed: {
+    score: function () {
+      let score = ''
+      if (this.playerSelection.score === undefined) {
+        score = ''
+      } else if (this.playerSelection.score > 0) {
+        score = 'Result: Winner!'
+      } else {
+        score = 'Result: Lose!'
+      }
+      console.log('Player.js', 'print score', this.playerSelection.username, score)
+      return score
+    }
+  }
 })
